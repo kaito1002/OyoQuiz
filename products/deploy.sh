64d27997edc7710c6bd924aace2329715b4cf420
Kaito <mail.kaito03@gmail.com>
2019-09-26 23:27:04 +0900

    fix deploy script

--- products/deploy.sh
+++ products/deploy.sh
@@ -1,14 +1,13 @@
 PROJECT="OyoQuiz"
 OS=$(uname)
 
-cd /home/ubuntu/${PROJECT}
+cd /home/ubuntu/${PROJECT}/docker
 sudo docker-compose down
 
 if [ ${OS} = "Linux" ]; then
   git fetch && git reset --hard origin/master
-  sed -i -e "s/#//g" docker/docker-compose.yml
+  sed -i -e "s/#//g" docker-compose.yml
 fi
 
-cd docker
 sudo docker-compose build
 sudo docker-compose up -d
