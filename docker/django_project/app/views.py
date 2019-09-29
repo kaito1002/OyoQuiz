a028ad96d63222bb0a236973010dba625d4d6453
Teriter <s1250133aizu@gmail.com>
2019-09-29 15:28:51 +0900

    web問題ページ作成(途中)

--- docker/django_project/app/views.py
+++ docker/django_project/app/views.py
@@ -6,3 +6,13 @@ def index(request):
         "sample_key": "sample_value"
     }
     return render(request, 'index.html', context)
+
+def exercise(request):
+    context = {
+        "select_1": 1,
+        "select_2": 2,
+        "select_3": 3,
+        "select_4": 4,
+        "ex_number": 5
+    }
+    return render(request, 'exercise.html', context)
