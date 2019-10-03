fd03c777b8632cd0256ffc3e9af3012c6796c73f
Kaito <mail.kaito03@gmail.com>
2019-10-03 13:46:02 +0900

    fix urls

--- docker/django_project/app/urls.py
+++ docker/django_project/app/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('exercise', views.exercise, name='exercise'),
-    path('answer', views.answer, name='answer'),
-    path('judge', views.judge, name='judge'),
+    path('exercise/<int:year>/<int:page_id>/', views.exercise, name='exercise'),
+    path('answer/<int:year>/<int:page_id>/', views.answer, name='answer'),
+    path('judge/', views.judge, name='judge'),
 ]
