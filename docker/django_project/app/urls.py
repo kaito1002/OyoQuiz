891062ed24e023b9f2b176b9afab7fbb4112108e
Teriter <s1250133aizu@gmail.com>
2019-10-03 10:17:39 +0900

    answer.html answer.css judge.html judge.css を作成

--- docker/django_project/app/urls.py
+++ docker/django_project/app/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('exercise', views.exercise, name='exercise'),
+    path('answer', views.answer, name='answer'),
+    path('judge', views.judge, name='judge'),
 ]
