0ec79f23c01bcc5e8c554c74c2a7f12f68566d56
Kaito <mail.kaito03@gmail.com>
2019-10-04 18:52:22 +0900

    modelを定義

--- docker/django_project/app/models.py
+++ docker/django_project/app/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+
+class Exam(models.Model):
+    year = models.IntegerField()             # 年度
+    type = models.IntegerField()             # 0: 午前, 1: 午後
+    question_number = models.IntegerField()  # 問題数
+
+    def __repr__(self):
+        return "{}: {}, {}".format(self.pk, self.year, self.question_number)
+
+    __str__ = __repr__
+
+
+class Question(models.Model):
+    statement = models.TextField()       # 問題文
+    description = models.TextField(null=True)     # 解説
+    answer = models.IntegerField()       # 正答 1 to 4
+    select1 = models.CharField(max_length=128)         # 選択肢1
+    select2 = models.CharField(max_length=128)         # 選択肢2
+    select3 = models.CharField(max_length=128)         # 選択肢3
+    select4 = models.CharField(max_length=128)         # 選択肢4
+    question_id = models.IntegerField(
+        unique=True,
+    )  # 問題番号
+    exam = models.ForeignKey(
+        Exam,
+        on_delete=models.CASCADE,
+        related_name="questions",
+    )
+
+    @property
+    def answer_str(self) -> str:
+        if self.answer == 1:
+            return "ア"
+        elif self.answer == 2:
+            return "イ"
+        elif self.answer == 3:
+            return "ウ"
+        else:  # 1 - 4 以外は考慮しない
+            return "エ"
+
+    def __repr__(self):
+        return "{}: {} => {}".format(self.pk, self.question_id, self.answer_str)
+
+    __str__ = __repr__
