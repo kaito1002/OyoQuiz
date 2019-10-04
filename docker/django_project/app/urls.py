0163ccc3b703ff1cb71a9f787fff52eb9b925736
Kaito <mail.kaito03@gmail.com>
2019-10-04 22:56:17 +0900

    正答率の実装

--- docker/django_project/app/urls.py
+++ docker/django_project/app/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('exercise/<int:year>/<str:type>/<int:page_id>/',
+    path('exercise/<int:year>/<int:e_type>/<int:page_id>/',
          views.exercise,
          name='exercise'),
-    path('answer/<int:year>/<str:type>/<int:page_id>/', views.answer, name='answer'),
-    path('judge/', views.judge, name='judge'),
+    path('answer/<int:year>/<int:e_type>/<int:page_id>/', views.answer, name='answer'),
+    path('judge/<int:year>/<int:e_type>/', views.judge, name='judge'),
     path('top/', views.top, name='top'),
 ]
--- docker/django_project/app/views.py
+++ docker/django_project/app/views.py
@@ -9,14 +9,14 @@ def index(request):
     return render(request, 'index.html', context)
 
 
-def exercise(request, year: int, type: str, page_id: int):
-    exam = Exam.objects.get(year=year, type=type)
+def exercise(request, year: int, e_type: int, page_id: int):
+    exam = Exam.objects.get(year=year, type=e_type)
     question = Question.objects.get(exam=exam, question_id=page_id)
 
     context = {
         "id": page_id,
         "year": year,
-        "type": type,
+        "type": e_type,
         "ex_number": page_id,
         "paragraph": question.statement,
         "selections": [
@@ -29,23 +29,44 @@ def exercise(request, year: int, type: str, page_id: int):
     return render(request, 'exercise.html', context)
 
 
-def answer(request, year: int, type: str, page_id: int):
-    exam = Exam.objects.get(year=year, type=type)
+def answer(request, year: int, e_type: int, page_id: int):
+    exam = Exam.objects.get(year=year, type=e_type)
     question = Question.objects.get(exam=exam, question_id=page_id)
+
+    # session result data
+    result = {}
+    if "result" in request.session.keys():
+        result = request.session["result"]
+
+    if str(year) not in result.keys():
+        result[str(year)] = {}
+
+    if str(e_type) not in result[str(year)].keys():
+        result[str(year)][str(e_type)] = {}
+
+    user_answer = 1  # なんらかの手段で受け取る
+    result[str(year)][str(e_type)][str(page_id)] = True if user_answer == question.answer else False
+    request.session["result"] = result
 
     context = {
         "next": page_id + 1,
         "year": year,
-        "type": type,
+        "type": e_type,
         "answer": question.answer_str,
         "description": question.description,
     }
     return render(request, 'answer.html', context)
 
 
-def judge(request):
+def judge(request, year: int, e_type: int):
+    results = list(request.session["result"][str(year)][str(e_type)].values())
+
+    num = len(results)
+    corrects_num = filter(lambda x: x, results)
+    corrects_num = len(list(corrects_num))
+
     context = {
-        "percentage": 80,
+        "percentage": 100 * (corrects_num / num),
         "comment": "大変良くできました."
     }
     return render(request, 'judge.html', context)
